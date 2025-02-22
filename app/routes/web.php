81eda41df23903d4c230a9e1cf49d27434590f55
yurio <monochro4s@gmail.com>
2025-02-22 14:05:53 +0900

    [ update ] preline用テストページ作成

--- app/routes/web.php
+++ app/routes/web.php
@@ -8,12 +8,18 @@ use App\Livewire\CreatePost;
 Route::get('/counter', Counter::class);
 Route::get('/create-post', CreatePost::class);
 
+Route::get('/', function () {
+    return view('index');
+});
 Route::get('/preline', function () {
     return view('preline');
+});
+Route::get('/preline-test', function () {
+    return view('preline-test');
 });
 Route::get('/test', function () {
     return view('index');
 });
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
